81ab2c88071ab9b68ac6b1638e901a4f6b669b70
sjcalabrese <erotavlas103@gmail.com>
2024-03-28 17:26:48 -0400

    Creating New Branch

--- Section 2 JavaScript Refresher/code/finished/assets/scripts/app.js
+++ Section 2 JavaScript Refresher/code/finished/assets/scripts/app.js
@@ -1,156 +1,33 @@
-// import { apiKey } from "./util.js";
+//! To import something you need to use keyword import {} and from to designate the pat
+//! it came from
 
-// import apiKey from "./util.js";
-// import { apiKey, abc as content } from "./util.js";
-// import * as util from "./util.js";
+//! to import a single variable
+//import { apikey } from "./util.js";
+//! to import a defaul export
+//import apiKey from "./util.js";
+//! to import multiple export items there are 2 ways
+//import { apikey, abc } from "./util.js";
+//! This imports it as a javascript object
+import * as util from "./util.js";
+//console.log(apiKey);
+//! To print out whwen using the javascript object way you will need to call the specific
+//! item from the object to log
+console.log(util.apikey);
+console.log(util.abc);
 
-// console.log(util.apiKey);
-// console.log(content);
+//!this funciton doesn't do anything until called
+function greet() {
+  console.log("Hello");
+}
+//! this is calling the function to perform it's action.
+greet();
 
-// const userMessage = "Hello World!!!";
-
-// console.log(userMessage);
-// console.log(userMessage);
-
-// function createGreeting(userName, message = "Hello!") {
-// console.log(userName);
-// console.log(message);
-//   return "Hi, I am " + userName + ". " + message;
-// }
-
-// const greeting1 = createGreeting("Max");
-// console.log(greeting1);
-
-// const greeting2 = createGreeting("Manuel", "Hello, what's up?");
-// console.log(greeting2);
-
-// export default (userName, message) => {
-//  console.log("Hello");
-//   return userName + message;
-// };
-
-// const user = {
-//   name: "Max",
-//   age: 34,
-//   greet() {
-//     console.log("Hello!");
-//     console.log(this.age);
-//   }
-// };
-
-// console.log(user.name);
-// user.greet();
-
-// class User {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-
-//   greet() {
-//     console.log("Hi!");
-//   }
-// }
-
-// const user1 = new User("Manuel", 35);
-// console.log(user1);
-// user1.greet();
-
-// const hobbies = ["Sports", "Cooking", "Reading"];
-// console.log(hobbies[0]);
-
-// hobbies.push("Working");
-// console.log(hobbies);
-
-// const index = hobbies.findIndex((item) => item === "Sports");
-
-// console.log(index);
-
-// const editedHobbies = hobbies.map((item) => ({ text: item }));
-// console.log(editedHobbies);
-
-// const [firstName, lastName] = ["Max", "Schwarzmüller"];
-
-// const firstName = userNameData[0];
-// const lastName = userNameData[1];
-
-// console.log(firstName);
-// console.log(lastName);
-
-// const { name: userName, age } = {
-//   name: "Max",
-//   age: 34
-// };
-
-// console.log(userName);
-// console.log(age);
-
-// const name = user.name;
-// const age = user.age;
-
-// const hobbies = ["Sports", "Cooking"];
-// const user = {
-//   name: "Max",
-//   age: 34
-// };
-
-// const newHobbies = ["Reading"];
-
-// const mergedHobbies = [...hobbies, ...newHobbies];
-// console.log(mergedHobbies);
-
-// const extendedUser = {
-//   isAdmin: true,
-//   ...user
-// };
-// console.log(extendedUser);
-
-// const password = prompt("Your password");
-
-// if (password === "Hello") {
-//   console.log("Hello works");
-// } else if (password === "hello") {
-//   console.log("hello works");
-// } else {
-//   console.log("Access not granted.");
-// }
-
-// const hobbies = ["Sports", "Cooking"];
-
-// for (const hobby of hobbies) {
-//   console.log(hobby);
-// }
-
-// const list = document.querySelector("ul");
-// list.remove();
-
-// function handleTimeout() {
-//   console.log("Timed out!");
-// }
-
-// const handleTimeout2 = () => {
-//   console.log("Timed out ... again!");
-// };
-
-// setTimeout(handleTimeout, 2000);
-// setTimeout(handleTimeout2, 3000);
-// setTimeout(() => {
-//   console.log("More timing out...");
-// }, 4000);
-
-// function init() {
-//   function greet() {
-//     console.log(“Hi!“);
-//   }
-
-//   greet();
-// }
-
-// init();
-
-// const message = “Hello”;
-
-const hobbies = ["Sports", "Cooking"];
-// hobbies = [];
-hobbies.push("Working");
-console.log(hobbies);
+/**
+ * This function is used to greet a user with their name and a message
+ * @param {string} userName - The name of the user to greet
+ * @param {string} message - The message to include in the greeting
+ */
+function greetWithParams(userName, message) {
+  console.log("Hello " + userName + " " + message);
+}
+greetWithParams("Sal", "World");
--- Section 2 JavaScript Refresher/code/finished/assets/scripts/util.js
+++ Section 2 JavaScript Refresher/code/finished/assets/scripts/util.js
@@ -1,4 +1,5 @@
-// export let apiKey = "adnasdoasflak1";
-export default "adnasdoasflak1";
-export let apiKey = "adnasdoasflak1";
+//! to make it available to other parts of the projet you need to mark it with export
+
+export default "asdfasdfasdf";
+export let apikey = "asdfasdfasdf";
 export let abc = "abc";
