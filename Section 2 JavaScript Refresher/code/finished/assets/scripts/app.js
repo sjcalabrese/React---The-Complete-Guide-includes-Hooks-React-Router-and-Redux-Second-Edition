f21b4ca09db049abd72e1d5af578d64b8fe90d26
sjcalabrese <erotavlas103@gmail.com>
2024-04-02 16:18:36 -0400

    function as values

--- Section 2 JavaScript Refresher/code/finished/assets/scripts/app.js
+++ Section 2 JavaScript Refresher/code/finished/assets/scripts/app.js
@@ -155,7 +155,33 @@ import * as util from "./util.js";
 //   console.log("Access not granted");
 // }
 
-const hobbies = ["Sports", "Cooking"];
-for (const hobby of hobbies) {
-  console.log(hobby);
+// const hobbies = ["Sports", "Cooking"];
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+//? Using function as value ***********************************************
+
+function handleTimeout() {
+  console.log("Timed Out!");
 }
+
+const handleTimeout2 = () => {
+  console.log("Timed Out Again!");
+};
+
+// pass the function withough the () at the end to pass is as a value and not to execute
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => {
+  console.log("more timeouts ");
+}, 4000);
+
+//! Can also create functions that take in functions
+function greeter(greetFn) {
+  greetFn();
+}
+
+greeter(() => {
+  console.log("hi");
+});
