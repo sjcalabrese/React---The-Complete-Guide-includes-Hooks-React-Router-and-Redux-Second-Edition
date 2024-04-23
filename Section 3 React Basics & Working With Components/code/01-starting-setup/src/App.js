061021b494c2a08cf9b9e401ca6079fb0aecc924
sjcalabrese <erotavlas103@gmail.com>
2024-04-23 16:36:03 -0400

    Updating Date output

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
@@ -6,16 +6,16 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 9412,
-      date: new Date(2023, 2, 28),
+      date: new Date(2023, 3, 28),
     },
-    { id: "e2", title: "New TV", amount: 799.99, date: new Date(2023, 2, 28) },
+    { id: "e2", title: "New TV", amount: 799.99, date: new Date(2023, 4, 28) },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2023, 2, 28),
     },
-    { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 2, 28) },
+    { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 1, 28) },
   ];
 
   return (
