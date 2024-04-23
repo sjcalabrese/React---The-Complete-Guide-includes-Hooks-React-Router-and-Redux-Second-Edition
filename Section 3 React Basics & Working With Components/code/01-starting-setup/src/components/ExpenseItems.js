a37e814ad68edfe2715d5dc6f83f6d05b4486c00
sjcalabrese <erotavlas103@gmail.com>
2024-04-23 16:22:27 -0400

    object destructuring props

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
@@ -1,6 +1,6 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+function ExpenseItem({date, title, amount}) {
 const expenseDate= new Date(2023, 2, 28);
 //! Date allows you to create a date object
 const expenseTitle = 'Car Insurance'; 
@@ -8,10 +8,10 @@ const expenseAmount = 294.67;
 
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
