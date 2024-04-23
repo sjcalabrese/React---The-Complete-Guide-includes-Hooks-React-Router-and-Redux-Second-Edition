659e81039e3ff1d756edd997fd65f1f87652bedb
sjcalabrese <erotavlas103@gmail.com>
2024-04-23 16:52:45 -0400

    Separating Date to a Component

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
@@ -1,21 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-//const expenseDate= new Date(2023, 2, 28);
-const month = props.date.toLocaleString('en-US', {month: 'long'});
-const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-const year = props.date.getFullYear();
-//! Date allows you to create a date object
-const expenseTitle = 'Car Insurance'; 
-const expenseAmount = 294.67;
+  //const expenseDate= new Date(2023, 2, 28);
+
+  //! Date allows you to create a date object
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
 
   return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-        </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
