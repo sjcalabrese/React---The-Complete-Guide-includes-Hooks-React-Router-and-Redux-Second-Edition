fe79ffc6e4d22475628b74d7d6df96b6fb57a1ee
sjcalabrese <erotavlas103@gmail.com>
2024-04-23 16:05:09 -0400

    props

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/ExpenseItems.js
@@ -1,6 +1,6 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 const expenseDate= new Date(2023, 2, 28);
 //! Date allows you to create a date object
 const expenseTitle = 'Car Insurance'; 
@@ -8,10 +8,10 @@ const expenseAmount = 294.67;
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
