ac124f3a1541479f5ff327ddee9ad2332e132b8f
sjcalabrese <erotavlas103@gmail.com>
2024-05-03 11:12:49 -0400

    Organizing Components

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItems";
+
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -17,30 +18,11 @@ function App() {
     },
     { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 1, 28) },
   ];
-
+  
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/Expenses/ExpenseItems.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/Expenses/ExpenseItems.js
@@ -1,21 +1,21 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   //const expenseDate= new Date(2023, 2, 28);
-
   //! Date allows you to create a date object
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+  //const expenseTitle = "Car Insurance";
+  //const expenseAmount = 294.67;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
