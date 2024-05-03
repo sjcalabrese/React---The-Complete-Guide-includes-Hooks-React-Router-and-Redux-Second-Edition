bce1ca59906fdb385c9ef503c028e15d5d115fa9
sjcalabrese <erotavlas103@gmail.com>
2024-05-03 11:17:20 -0400

    updating functions to use arrow syntax

--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/App.js
@@ -1,24 +1,43 @@
 
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 9412,
-      date: new Date(2023, 3, 28),
-    },
-    { id: "e2", title: "New TV", amount: 799.99, date: new Date(2023, 4, 28) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2023, 2, 28),
-    },
-    { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 1, 28) },
-  ];
-  
+// function App() {
+//   const expenses = [
+//     {
+//       id: "e1",
+//       title: "Toilet Paper",
+//       amount: 9412,
+//       date: new Date(2023, 3, 28),
+//     },
+//     { id: "e2", title: "New TV", amount: 799.99, date: new Date(2023, 4, 28) },
+//     {
+//       id: "e3",
+//       title: "Car Insurance",
+//       amount: 294.67,
+//       date: new Date(2023, 2, 28),
+//     },
+//     { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 1, 28) },
+//   ];
+
+//! Alternative way to create a function
+  const App = () => {
+    const expenses = [
+      {
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 9412,
+        date: new Date(2023, 3, 28),
+      },
+      { id: "e2", title: "New TV", amount: 799.99, date: new Date(2023, 4, 28) },
+      {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2023, 2, 28),
+      },
+      { id: "e4", title: "New Desk", amount: 450.0, date: new Date(2023, 1, 28) },
+    ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
--- Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/UI/Card.js
+++ Section 3 React Basics & Working With Components/code/01-starting-setup/src/components/UI/Card.js
@@ -1,4 +1,4 @@
-function Card(props) {
+const Card = (props) => {
   const classes = 'card ' + props.className;
 
   return <div className={classes}>{props.children}</div>;
